a3c68fef1dd449162c3cbc356a319c81532d5902
ikarian7 <scott.marchant7@gmail.com>
2022-01-12 16:14:18 +0100

    automated text

--- Assets/Scripts/Environment/PrimaryReactor.cs
+++ Assets/Scripts/Environment/PrimaryReactor.cs
@@ -6,41 +6,41 @@ public class PrimaryReactor : MonoBehaviour
 {
     public PrimaryButtonWatcher watcher;
     public bool IsPressed = false; // used to display button state in the Unity Inspector window
-    public Vector3 descentSpeed = new Vector3(1, 5, 1);
-    public float descentDuration = 0.25f; // seconds
-    private Vector3 offPosition;
-    private Vector3 onPosition;
-    private Coroutine descent;
-    [SerializeField]
-    private GameObject bushes;
+    //public Vector3 descentSpeed = new Vector3(1, 5, 1);
+    //public float descentDuration = 0.25f; // seconds
+    //private Vector3 offPosition;
+    //private Vector3 onPosition;
+    //private Coroutine descent;
+    //[SerializeField]
+    //private GameObject bushes;
 
     void Start()
     {
         watcher.primaryButtonPress.AddListener(onPrimaryButtonEvent);
-        offPosition = bushes.transform.position;
-        onPosition = Vector3.Scale(descentSpeed, offPosition);
+        //offPosition = bushes.transform.position;
+        //onPosition = Vector3.Scale(descentSpeed, offPosition);
     }
 
     public void onPrimaryButtonEvent(bool pressed)
     {
         IsPressed = pressed;
-        if (descent != null)
-            StopCoroutine(descent);
-        if (pressed)
-            bushes.SetActive(true);
-            descent = StartCoroutine(AnimateDescent(bushes.transform.position, onPosition));
-        //else
-        //    descent = StartCoroutine(AnimateDescent(this.transform.position, offPosition));
+        //if (descent != null)
+        //    StopCoroutine(descent);
+        //if (pressed)
+        //    bushes.SetActive(true);
+        //    descent = StartCoroutine(AnimateDescent(bushes.transform.position, onPosition));
+        ////else
+        ////    descent = StartCoroutine(AnimateDescent(this.transform.position, offPosition));
     }
 
-    public IEnumerator AnimateDescent(Vector3 fromPosition, Vector3 toPosition)
-    {
-        float t = 0;
-        while (t < descentDuration)
-        {
-            transform.position = Vector3.Lerp(fromPosition, toPosition, t / descentDuration);
-            t += Time.deltaTime;
-            yield return null;
-        }
-    }
+    //public IEnumerator AnimateDescent(Vector3 fromPosition, Vector3 toPosition)
+    //{
+    //    float t = 0;
+    //    while (t < descentDuration)
+    //    {
+    //        transform.position = Vector3.Lerp(fromPosition, toPosition, t / descentDuration);
+    //        t += Time.deltaTime;
+    //        yield return null;
+    //    }
+    //}
 }
--- Assets/Scripts/UI/DialogueManager.cs
+++ Assets/Scripts/UI/DialogueManager.cs
@@ -7,12 +7,35 @@ public class DialogueManager : MonoBehaviour {
     public TextMeshProUGUI beeText;
 
     [SerializeField]
-    private Queue<string> sentences;
+    private List<string> sentences;
+
+    private int sentenceIndex = 0;
+
+    private int amountOfSecondsTillNext = 20;
+    private float timer = 0.0f;
+    private int timerInSeconds = 0;
+    private int amountOfMSInASecond = 60;
+    private bool counting = false;
+    private int zero = 0;
 
     bool pushedOnce = false;
 
     void Awake() {
-        sentences = new Queue<string>();
+        sentences = new List<string>();
+    }
+
+    void Update()
+    {
+        if (sentenceIndex == 1 || sentenceIndex == 2)
+        {
+            counting = true;
+        }
+
+        if (counting)
+        {
+            TimerCheck();
+        }
+
     }
 
     public void StartBeeText(Dialogue dialogue) {
@@ -21,7 +44,7 @@ public class DialogueManager : MonoBehaviour {
             sentences.Clear();
 
             foreach(string sentence in dialogue.sentences) {
-                sentences.Enqueue(sentence);
+                sentences.Add(sentence);
             }
 
             DisplayNextSentence();
@@ -29,13 +52,34 @@ public class DialogueManager : MonoBehaviour {
     }
 
     public void DisplayNextSentence() {
-        if(sentences.Count == 0) {
+        if(sentenceIndex > sentences.Count) {
             EndDialogue();
             return;
         }
 
-        string sentence = sentences.Dequeue();
+        string sentence = sentences[sentenceIndex];
         beeText.text = sentence;
+        sentenceIndex++;
+        
+    }
+
+    public void TimerCheck()
+    {
+        timer += Time.deltaTime;
+        timerInSeconds = (int)timer % amountOfMSInASecond;
+
+        if (timerInSeconds >= amountOfSecondsTillNext)
+        {
+            ResetTimer();
+            DisplayNextSentence();
+        }
+    }
+
+    private void ResetTimer()
+    {
+        counting = false;
+        timer = zero;
+        timerInSeconds = zero;
     }
 
     void EndDialogue() {
